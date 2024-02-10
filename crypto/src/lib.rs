6d0d54df46f406aa6d7644ae4ce44c58ee2eb102
Tyler Adam Martinez <tylermartinez@my.unt.edu>
2024-02-09 23:58:00 -0600

    [lib] Add doc comments

--- crypto/src/lib.rs
+++ crypto/src/lib.rs
@@ -6,19 +6,25 @@ use rsa::{
 use serde::{Deserialize, Deserializer, Serialize, Serializer};
 use std::time::{SystemTime, UNIX_EPOCH};
 
+/// Represents a RSA key pair with a unique identifier and expiry timestamp.
 #[derive(Serialize, Deserialize)]
 pub struct KeyPair {
+    /// A unique identifier for the key pair.
     pub kid: String,
+    /// The RSA public key, serialized and deserialized as PEM format.
     #[serde(
         serialize_with = "serialize_rsa_public_key",
         deserialize_with = "deserialize_rsa_public_key"
     )]
     pub public_key: RsaPublicKey,
+    /// The RSA private key, which is excluded from serialization and deserialization.
     #[serde(skip)]
     pub private_key: Option<RsaPrivateKey>,
+    /// The expiry timestamp of the key pair in UNIX timestamp format.
     pub expiry: u64,
 }
 
+/// Serializes an `RsaPublicKey` to a PEM format string for storage or transmission.
 fn serialize_rsa_public_key<S>(key: &RsaPublicKey, serializer: S) -> Result<S::Ok, S::Error>
 where
     S: Serializer,
@@ -27,6 +33,7 @@ where
     serializer.serialize_str(&pem)
 }
 
+/// Deserializes an `RsaPublicKey` from a PEM format string.
 fn deserialize_rsa_public_key<'de, D>(deserializer: D) -> Result<RsaPublicKey, D::Error>
 where
     D: Deserializer<'de>,
@@ -36,6 +43,17 @@ where
 }
 
 impl KeyPair {
+    /// Creates a new `KeyPair` with the specified unique identifier (`kid`), key size, and expiry duration.
+    ///
+    /// # Parameters
+    ///
+    /// * `kid` - A unique identifier for the key pair.
+    /// * `key_size` - The size of the RSA key in bits.
+    /// * `expiry_duration` - The duration from the current time after which the key pair expires, in seconds.
+    ///
+    /// # Returns
+    ///
+    /// A new `KeyPair` instance.
     pub fn new(kid: &str, key_size: usize, expiry_duration: u64) -> Self {
         let mut rng = OsRng;
         let private_key = RsaPrivateKey::new(&mut rng, key_size).expect("Failed to generate a key");
@@ -56,6 +74,11 @@ impl KeyPair {
         }
     }
 
+    /// Checks whether the key pair has expired based on the current system time.
+    ///
+    /// # Returns
+    ///
+    /// `true` if the key pair has expired, `false` otherwise.
     pub fn is_expired(&self) -> bool {
         let current_time = SystemTime::now()
             .duration_since(UNIX_EPOCH)
@@ -65,16 +88,3 @@ impl KeyPair {
         self.expiry <= current_time
     }
 }
-
-/*
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
-*/
