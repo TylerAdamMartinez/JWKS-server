1fe458eca3aefc5a06ac7d6c386e59767370ff0b
Tyler Adam Martinez <tylermartinez@my.unt.edu>
2024-02-09 23:53:29 -0600

    [crypto] Add init RSA struct

--- crypto/src/lib.rs
+++ crypto/src/lib.rs
@@ -1,7 +1,72 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use rand::rngs::OsRng;
+use rsa::{
+    pkcs8::{FromPublicKey, ToPublicKey},
+    RsaPrivateKey, RsaPublicKey,
+};
+use serde::{Deserialize, Deserializer, Serialize, Serializer};
+use std::time::{SystemTime, UNIX_EPOCH};
+
+#[derive(Serialize, Deserialize)]
+pub struct KeyPair {
+    pub kid: String,
+    #[serde(
+        serialize_with = "serialize_rsa_public_key",
+        deserialize_with = "deserialize_rsa_public_key"
+    )]
+    pub public_key: RsaPublicKey,
+    #[serde(skip)]
+    pub private_key: Option<RsaPrivateKey>,
+    pub expiry: u64,
 }
 
+fn serialize_rsa_public_key<S>(key: &RsaPublicKey, serializer: S) -> Result<S::Ok, S::Error>
+where
+    S: Serializer,
+{
+    let pem = key.to_public_key_pem().map_err(serde::ser::Error::custom)?;
+    serializer.serialize_str(&pem)
+}
+
+fn deserialize_rsa_public_key<'de, D>(deserializer: D) -> Result<RsaPublicKey, D::Error>
+where
+    D: Deserializer<'de>,
+{
+    let pem = String::deserialize(deserializer)?;
+    RsaPublicKey::from_public_key_pem(&pem).map_err(serde::de::Error::custom)
+}
+
+impl KeyPair {
+    pub fn new(kid: &str, key_size: usize, expiry_duration: u64) -> Self {
+        let mut rng = OsRng;
+        let private_key = RsaPrivateKey::new(&mut rng, key_size).expect("Failed to generate a key");
+        let public_key = RsaPublicKey::from(&private_key);
+
+        let private_key = Some(private_key);
+        let expiry = SystemTime::now()
+            .duration_since(UNIX_EPOCH)
+            .expect("Time went backwards")
+            .as_secs()
+            + expiry_duration;
+
+        KeyPair {
+            kid: kid.to_owned(),
+            public_key,
+            private_key,
+            expiry,
+        }
+    }
+
+    pub fn is_expired(&self) -> bool {
+        let current_time = SystemTime::now()
+            .duration_since(UNIX_EPOCH)
+            .expect("Time went backwards")
+            .as_secs();
+
+        self.expiry <= current_time
+    }
+}
+
+/*
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -12,3 +77,4 @@ mod tests {
         assert_eq!(result, 4);
     }
 }
+*/
